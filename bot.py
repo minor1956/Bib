86bedb24a6857e05213fbc57ac49acee7731dbfc
minor1956 <53707304+minor1956@users.noreply.github.com>
2019-08-04 22:04:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
+import os
+from flask import Flask, request
 import telebot
 import schedule
 import time
 import random
 
-bot = telebot.TeleBot('686570673:AAFfCDwWnjQ-qj8DyNeTYk-Uax7NnVdBHGo')
+TOKEN = '686570673:AAFfCDwWnjQ-qj8DyNeTYk-Uax7NnVdBHGo'
+bot = telebot.TeleBot(TOKEN)
 # name = "black_list_jpg"
+server = Flask(__name__)
+
 ok = False
 name = "piuuo"
 anya = ['Анна', 'Аня', 'Анечка', 'Анюта', 'Анюточка', 'Аннушка', 'Анюточечка', 'Анюша', 'Анюшенька', 'Анюшечка']
@@ -66,5 +71,20 @@ def repeat_all_messages(message):  # Название функции не игр
         bot.send_message(message.chat.id, 'Ты не Анечка!')
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+# if __name__ == '__main__':
+#     bot.polling(none_stop=True)
