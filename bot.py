2d984ba2adc6d715ff47131d80ac6c1988bc8d24
minor1956 <53707304+minor1956@users.noreply.github.com>
2019-08-04 21:44:19 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,70 @@
+import telebot
+import schedule
+import time
+import random
+
+bot = telebot.TeleBot('686570673:AAFfCDwWnjQ-qj8DyNeTYk-Uax7NnVdBHGo')
+# name = "black_list_jpg"
+ok = False
+name = "piuuo"
+anya = ['Анна', 'Аня', 'Анечка', 'Анюта', 'Анюточка', 'Аннушка', 'Анюточечка', 'Анюша', 'Анюшенька', 'Анюшечка']
+end = ['киса', 'кисонька', 'кисунечка', 'кисонька', 'кисулечка', 'кисуленька', '', 'солнышко', 'деточка', 'детка']
+smiles = ['😘', '🥰', '😍', '😚', '☺️', '😻', '😽', '💞', '💋', '♥️']
+
+
+@bot.message_handler(commands=['start', 'restart'])
+def hello(message):
+    if message.from_user.username == name:
+        global ok
+        ok = True
+        bot.send_message(message.chat.id, 'Привет, Аня)')
+
+        def morning():
+            bot.send_message(message.chat.id, anya[random.randint(0, len(anya) - 1)] + ", доброго тебе утречка, " + end[
+                random.randint(0, len(end) - 1)] +
+                             smiles[random.randint(0, len(smiles) - 1)])
+
+        def day():
+            bot.send_message(message.chat.id,
+                             anya[random.randint(0, len(anya) - 1)] + ", доброго тебе денёчка, " + end[
+                                 random.randint(0, len(end) - 1)] +
+                             smiles[random.randint(0, len(smiles) - 1)])
+
+        def evening():
+            bot.send_message(message.chat.id,
+                             anya[random.randint(0, len(anya) - 1)] + ", доброго тебе вечерочка, " + end[
+                                 random.randint(0, len(end) - 1)] +
+                             smiles[random.randint(0, len(smiles) - 1)])
+
+        def night():
+            bot.send_message(message.chat.id, anya[random.randint(0, len(anya) - 1)] + ", доброй тебе ночки, " + end[
+                random.randint(0, len(end) - 1)] +
+                             smiles[random.randint(0, len(smiles) - 1)])
+
+        # schedule.every().day.at("08:00").do(morning)
+        # schedule.every().day.at("12:00").do(day)
+        # schedule.every().day.at("18:00").do(evening)
+        # schedule.every().day.at("00:00").do(night)
+        schedule.every(2).seconds.do(morning)
+        schedule.every(4).seconds.do(day)
+        schedule.every(6).seconds.do(evening)
+        schedule.every(7).seconds.do(night)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    else:
+        bot.send_message(message.chat.id, 'Ты не Анечка!')
+
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
+    if message.from_user.username == name:
+        bot.reply_to(message, '😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘😘')
+        print(message.text)
+    else:
+        bot.send_message(message.chat.id, 'Ты не Анечка!')
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
