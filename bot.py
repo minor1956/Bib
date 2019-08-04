4f295d3fdb4c2aa4687aa22419084e8864631299
minor1956 <53707304+minor1956@users.noreply.github.com>
2019-08-05 00:05:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -87,4 +87,8 @@ def webhook():
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    try:
+        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
+    except Exception as e:
+        time.sleep(15)
