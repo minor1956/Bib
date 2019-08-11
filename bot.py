3eb74860ea95b3f96a12bbbd5e5f02baf9010ecb
minor1956 <53707304+minor1956@users.noreply.github.com>
2019-08-11 13:45:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,7 +101,7 @@ def repeat_all_messages(message):  # Название функции не игр
             bot.send_sticker(message.chat.id, sticker_id)
         elif message.text == 'История из жития Витька(реальная!!!)' and message.from_user.username != 'piuuo':
             bot.send_message(message.chat.id, stories[random.randint(0, len(stories) - 1)])
-        elif message.text == 'Кисонька для кисоньки😽' and message.from_user.username != 'piuuo':
+        elif message.text == 'Кисонька для кисоньки😽':  # and message.from_user.username != 'piuuo':
             try:
                 bot.send_photo(message.chat.id, 'https://random.cat/view/' + str(random.randint(1, 1677)))
             except Exception as e:
@@ -152,7 +152,6 @@ def photo_message(message):
         if message.from_user.username != 'piuuo':
             bot.send_message(VITKA, message.from_user.first_name + " прислала фото")
         bot.send_message(message.chat.id, 'Ты не Анечка!')
-
 
 
 @server.route('/' + TOKEN, methods=['POST'])
